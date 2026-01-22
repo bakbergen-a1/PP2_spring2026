159100a1db5e708d756c5993964b7a39588b8757
Aigerim <bakbergenaigerim13@gmail.com>
2026-01-22 18:59:27 +0500

    working with numbers and different operations

---
+++ practice1/numbers.py
@@ -0,0 +1,18 @@
+#working with numbers and different operations
+x=20
+y=5
+
+#addition 
+print(x+y)
+
+#subtraction
+print(x-y)
+
+#multiplication
+print(x*y)
+
+#float division
+print(x/y)
+
+#integer division 
+print(x//y)
