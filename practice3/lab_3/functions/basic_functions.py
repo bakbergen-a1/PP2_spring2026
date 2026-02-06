7b2f3ec9d1dc89720ca92ac83e228b4baf7f449c
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-07 01:43:07 +0500

    Basic function definition and calling

---
+++ practice3/lab_3/functions/basic_functions.py
@@ -0,0 +1,19 @@
+# Basic function definition and calling
+
+# Define a simple function
+def greet():
+    print("Hello, world!")
+
+# Call the function
+greet()
+
+# Function with parameters
+def greet_person(name):
+    print(f"Hello, {name}!")
+
+greet_person("Aigerim")
+
+# Function with numbers
+def area(a,b):
+    print(a*b)
+area(3,4)
