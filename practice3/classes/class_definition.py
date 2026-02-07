0d783e7f735f3b7916587961667eb0afa9e3324b
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 01:09:26 +0500

    class definition

---
+++ practice3/classes/class_definition.py
@@ -0,0 +1,25 @@
+class Person:
+    pass 
+
+p = Person()
+print(p)
+
+class Dog:
+    pass
+
+d = Dog()
+
+class Student:
+    pass
+
+s = Student()
+
+class Car:
+    pass
+
+c = Car()
+
+class Book:
+    pass
+
+b = Book()
