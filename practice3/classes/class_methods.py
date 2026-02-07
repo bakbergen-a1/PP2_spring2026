8595b3baeb4fec587bef70e50b9c0b59c5443fab
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 01:14:47 +0500

    class variables

---
+++ practice3/classes/class_methods.py
@@ -0,0 +1,29 @@
+class Person:
+    def greet(self):
+        print("Hello")
+
+Person().greet()
+
+class Student:
+    def study(self):
+        print("Studying")
+
+Student().study()
+
+class Car:
+    def drive(self):
+        print("Driving")
+
+Car().drive()
+
+class Phone:
+    def call(self):
+        print("Calling")
+
+Phone().call()
+
+class Teacher:
+    def teach(self):
+        print("Teaching")
+
+Teacher().teach()
---
+++ practice3/classes/class_variables.py
@@ -0,0 +1,24 @@
+class University:
+    name = "KBTU"
+
+print(University.name)
+
+class Car:
+    wheels = 4
+
+print(Car.wheels)
+
+class Country:
+    capital = "Astana"
+
+print(Country.capital)
+
+class Phone:
+    brand = "Apple"
+
+print(Phone.brand)
+
+class Game:
+    type = "Online"
+
+print(Game.type)
