120744add3e835a98da2152bc92a69669ab0618e
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 00:41:43 +0500

    basic lambda

---
+++ practice3/lambda/lambda_basics.py
@@ -0,0 +1,14 @@
+x = lambda a: a + 10
+print(x(5))
+
+y = lambda a, b: a * b
+print(y(3, 4))
+
+square = lambda n: n * n
+print(square(6))
+
+even = lambda n: n % 2 == 0
+print(even(8))
+
+name = lambda s: s.upper()
+print(name("python"))
