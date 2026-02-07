87c0a49f06ca57c682c5be37c0aea41559ab822b
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 00:27:16 +0500

    function with arguments

---
+++ practice3/functions/function_arguments.py
@@ -0,0 +1,25 @@
+def student(name, age):
+    print(name, age)
+
+student("Ali", 18)
+
+def add(a, b):
+    print(a + b)
+
+add(3, 5)
+
+def country(name="Kazakhstan"):
+    print(name)
+
+country()
+country("Japan")
+
+def food(f1, f2):
+    print(f1, f2)
+
+food("apple", "banana")
+
+def info(city, year):
+    print(city, year)
+
+info("Almaty", 2024)
