9bb9f8e64de842edc2aa35ba2c1e9d2b459d476b
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 00:28:54 +0500

    working with return values

---
+++ practice3/functions/return_values.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
+
+def square(x):
+    return x * x
+
+print(square(4))
+
+def is_even(n):
+    return n % 2 == 0
+
+print(is_even(6))
+
+def full_name(a, b):
+    return a + " " + b
+
+print(full_name("Aigerim", "B."))
+
+def max_num(a, b):
+    return a if a > b else b
+
+print(max_num(10, 7))
