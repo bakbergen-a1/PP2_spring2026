441867c6970d549083d945e92e786261d4c66d60
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-08 00:39:40 +0500

    args and kwargs

--- practice3/functions/args_kwargs.py
+++ practice3/functions/args_kwargs.py
@@ -14,3 +14,10 @@ def info(**data):
 
 info(name="Aigerim", age=18)
 
+def my_function(**myvar):
+  print("Type:", type(myvar))
+  print("Name:", myvar["name"])
+  print("Age:", myvar["age"])
+  print("All data:", myvar)
+
+my_function(name = "Aigerim", age = 18, city = "Almaty ")
