5df5f3daa0f6c51ff02c7eef8baadb8e50d41d27
Aigerim <bakbergenaigerim13@gmail.com>
2026-02-21 18:17:11 +0500

    math operators

---
+++ practice4/math/my_math.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+#Square root
+print(math.sqrt(25))
+
+#Rounding
+print(math.ceil(4.3))
+print(math.floor(4.8))
+
+#Pi value
+print(math.pi)
+
+#Random number
+print(random.randint(1, 10))
+
+#Random item selection
+colors = ["red", "blue", "green"]
+print(random.choice(colors))
