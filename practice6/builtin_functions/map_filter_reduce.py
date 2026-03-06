4dc3e5391ae15175bd1116cd8a29533ca3738963
Aigerim <bakbergenaigerim13@gmail.com>
2026-03-06 23:20:45 +0500

    Add map, filter, and reduce examples (5 exercises) for practice

--- practice6/builtin_functions/map_filter_reduce.py
+++ practice6/builtin_functions/map_filter_reduce.py
@@ -2,22 +2,22 @@ from functools import reduce
 
 numbers = [1, 2, 3, 4, 5, 6]
 
-# 1. map() - возведение в квадрат
+# 1. map() - squaring
 squares = list(map(lambda x: x**2, numbers))
 print("Squares:", squares)
 
-# 2. map() - удвоение чисел
+# 2. map() - doubling numbers
 doubled = list(map(lambda x: x*2, numbers))
 print("Doubled:", doubled)
 
-# 3. filter() - оставить только чётные
+# 3. filter() - leave only even numbers
 even = list(filter(lambda x: x % 2 == 0, numbers))
 print("Even numbers:", even)
 
-# 4. filter() - оставить числа больше 3
+# 4. filter() - leave numbers greater than 3
 greater_than_3 = list(filter(lambda x: x > 3, numbers))
 print("Numbers > 3:", greater_than_3)
 
-# 5. reduce() - сумма всех чисел
+# 5. reduce() - the sum of all the numbers
 total = reduce(lambda x, y: x + y, numbers)
 print("Sum:", total)
